e31b33232d767827c72781eb44c9ce6ca38530bf
duzhg56980 <guokey1996@gmail.com>
2024-02-19 12:13:52 +0800

    init commit

---
+++ 272463-1.1.72.android/FunctionUse469.java
@@ -0,0 +1,6 @@
+import com.alibaba.fastjson.parser.JavaBeanInfo;
+public class FunctionUse469 {
+public void funcUse() {
+JavaBeanInfo javabeaninfo = new JavaBeanInfo();
+}
+}
---
+++ 272463-1.1.72.android/FunctionUse604.java
@@ -0,0 +1,6 @@
+import com.alibaba.fastjson.JSONArray;
+public class FunctionUse604 {
+public void funcUse() {
+JSONArray jsonarray = new JSONArray();
+}
+}
